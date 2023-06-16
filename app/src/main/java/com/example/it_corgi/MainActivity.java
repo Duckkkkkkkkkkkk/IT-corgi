d3b4c1d700bc2f28afd83fef08d6b42f534328f4
Duckkkkkkkkkkkk <alkim345@mail.ru>
2023-06-16 21:22:53 +0300

    IT-corgi

---
+++ app/src/main/java/com/example/it_corgi/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.it_corgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+import com.example.it_corgi.adapter.CategoryAdapter;
+import com.example.it_corgi.model.Category;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры")); // можно добавить дополнительные категории
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList){
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/it_corgi/model/Category.java
@@ -0,0 +1,28 @@
+package com.example.it_corgi.model;
+
+public class Category {
+
+    int id;
+    String title;
+
+    public Category(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

23c6b0a63ad7d7e92166687bdabcf1804e4014a6
Polik5 <88332126+Polik5@users.noreply.github.com>
2023-06-21 00:00:58 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/it_corgi/CoursePage.java
@@ -0,0 +1,31 @@
+package com.example.it_corgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CoursePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_page);
+        ConstraintLayout courseBg = findViewById(R.id.coursePageBg);
+        ImageView courseImage = findViewById(R.id.coursePageImage);
+        TextView courseTitle = findViewById(R.id.coursePageTitle);
+        TextView courseDate = findViewById(R.id.coursePageDate);
+        TextView courseLevel = findViewById(R.id.coursePageLevel);
+        TextView courseText = findViewById(R.id.coursePageText);
+        courseBg.setBackgroundColor(getIntent().getIntExtra("courseBg", 0));
+        courseImage.setImageResource(getIntent().getIntExtra("courseImage", 0));
+        courseTitle.setText(getIntent().getStringExtra("courseTitle"));
+        courseDate.setText(getIntent().getStringExtra("courseDate"));
+        courseLevel.setText(getIntent().getStringExtra("courseLevel"));
+        courseText.setText(getIntent().getStringExtra("courseText"));
+
+
+    }
+}

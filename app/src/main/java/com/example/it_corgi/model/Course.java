f12e4fbaaf86a4b205585b811f95fcced67223bd
Polik5 <88332126+Polik5@users.noreply.github.com>
2023-06-20 23:59:29 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/it_corgi/model/Course.java
@@ -0,0 +1,72 @@
+package com.example.it_corgi.model;
+
+public class Course {
+    int id;
+    String img, title, date, level, color, text;
+
+    public Course(int id, String img, String title, String date, String level, String color, String text) {
+        this.id = id;
+        this.img = img;
+        this.title = title;
+        this.date = date;
+        this.level = level;
+        this.color = color;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}

f0c5dc304ef7c0d208bd1cc7403743382e132564
Polik5 <88332126+Polik5@users.noreply.github.com>
2023-06-21 00:02:19 +0300

    MainActivity.java

--- app/src/main/java/com/example/it_corgi/MainActivity.java
+++ app/src/main/java/com/example/it_corgi/MainActivity.java
@@ -9,15 +9,17 @@ import android.os.Bundle;
 import java.util.List;
 
 import com.example.it_corgi.adapter.CategoryAdapter;
+import com.example.it_corgi.adapter.CourseAdapter;
 import com.example.it_corgi.model.Category;
+import com.example.it_corgi.model.Course;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView categoryRecycler;
+    RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
-
+    CourseAdapter courseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +33,23 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(4, "Прочее"));
 
         setCategoryRecycler(categoryList);
-    }
 
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "java", "Профессия Java\nразработчик", "1 сентября", "junior", "#424345", "9-месячная программа обучения, во время которой ученик получает базовые знания алгоритмов, на практике освоит язык Java и сможет создавать универсальные программы для большинства современных платформ.")); // прописываем курсы
+        courseList.add(new Course(2, "python", "Профессия Python\nразработчик", "1 сентября", "junior", "#9FA52D", "9-месячная программа обучения, во время которой ученик на практике научится понимать фундаментальные алгоритмы и использовать их для написания сайтов, приложений, нейросетей иTelegram-бота."));
+        courseList.add(new Course(3, "front", "Профессия Frontend\nразработчик", "1 августа", "junior", "#F1BF26", "9-месячная программа обучения, во время которой ученик получает базовые знания и на практике учится создавать сайты и приложения, проектировать интерфейсы и работать со сложными инструментами Frontend-разработчика."));
+        courseList.add(new Course(4, "cplusplus", "Профессия С++\nразработчик", "1 октября", "junior", "#669AD3", "9-месячная программа обучения, во время которой ученик осваивает создание программы, игры, драйвера для устройств, изучает основы этого языка и на практике реализовывает графические интерфейсы."));
+        setCourseRecycler(courseList);
+    }
+    private void setCourseRecycler(List<Course> courseList){
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
+    }
     private void setCategoryRecycler(List<Category> categoryList){
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
